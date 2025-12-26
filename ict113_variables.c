e5bae329b585405fd185c9a2b4a91494abc41506
Prisca Njovu <pnjovu256@gmail.com>
2025-12-26 17:32:14 +0200

    Add files via upload

---
+++ ict113_variables.c
@@ -0,0 +1,16 @@
+
+	
+	#include <stdio.h>
+
+int main() {
+
+    int age = 24;
+    char name[25] = "prisca";
+    double gpa = 4.5;
+
+    printf("Student Age: %d\n", age);
+    printf("Student Name: %s\n", name);
+    printf("Your GPA: %f\n", gpa);
+
+    return 0;
+}
